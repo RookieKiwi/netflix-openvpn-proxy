17594fbeafd1c03fded50af8cac4de84a2478bf0
RookieKiwi <james@jameswhyte.com>
2021-03-01 15:59:58 +1300

    added more proxy servers and cdns

--- route.sh
+++ route.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "re-routing local ports to main interface while VPN is connected"
-ip route flush table 128
+ip route flush table 128 >>/dev/null
 ip rule add from $(ip route get 1 | grep -Po '(?<=src )(\S+)') table 128
 ip route add table 128 to $(ip route get 1 | grep -Po '(?<=src )(\S+)')/32 dev $(ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)')
 ip route add table 128 default via $(ip -4 route ls | grep default | grep -Po '(?<=via )(\S+)')
